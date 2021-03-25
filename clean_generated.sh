7d05b9956729bd0d478f0b88b272ef152764e930
Marco Ponzi <marco.ponzi@gmail.com>
2021-03-25 09:07:42 +0100

    df black text

--- clean_generated.sh
+++ clean_generated.sh
@@ -1,6 +1,9 @@
 cat texts/generated/donald_fisk/generated-voynich-manuscript.txt | grep -v '[A-Z]'> texts/generated/clean/df
 
 cat texts/generated/donald_fisk/generated-voynich-manuscript.txt | sed -e '1,2703d'> texts/generated/clean/df_text_q20
+
+cat texts/generated/donald_fisk/generated-voynich-manuscript.txt | sed -e '4203,$d'| sed -e '1,3067d'> \
+   texts/generated/clean/df_blck_txt
  
 grep -v '^#' texts/generated/timm_and_schinner/generated_text.txt > texts/generated/clean/ts
 
