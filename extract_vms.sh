8c6320e79c9aa8b50e3d04291315b0e609584168
Marco Ponzi <marco.ponzi@gmail.com>
2021-04-26 15:50:05 +0200

    text split by illustration type ivtt +I

--- extract_vms.sh
+++ extract_vms.sh
@@ -48,40 +48,48 @@ $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QH +PC +LA >
 $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QH +PD +LA >> texts/vms/sections_unk/Unknown
 $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IH +LA > texts/vms/sections_unk/EVA_HerbA
 
-$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IP > texts/vms/sections/EVA_Pharma
-$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IP > texts/vms/sections_unk/EVA_Pharma
-
 $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IH +LB > texts/vms/sections/EVA_HerbB
 $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IH +LB > texts/vms/sections_unk/EVA_HerbB
 
 # QH PE/F f65r/v belong to language B, see https://www.voynich.ninja/thread-3192-post-37291.html#pid37291
 $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QH +PE >> texts/vms/sections/EVA_HerbB
 $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QH +PF >> texts/vms/sections/EVA_HerbB
+
 # f66r
 $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QH +PG >> texts/vms/sections/EVA_HerbB
+
+for dir in sections sections_unk illustrations
+do
+  $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IP > texts/vms/$dir/EVA_Pharma
+
+  $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IA > texts/vms/$dir/EVA_AstroCZ
+  $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IC >> texts/vms/$dir/EVA_AstroCZ
+  $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IZ >> texts/vms/$dir/EVA_AstroCZ
+
+  # +QT Quire13 / Bio
+  $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QM > texts/vms/$dir/EVA_Q13
+
+  # +QT Quire20
+  $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QT > texts/vms/$dir/EVA_Q20
+done
+
+
+# "illustrations" splits pages purely by illustration type +I
+$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IH > texts/vms/illustrations/EVA_Herbal
+$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IT >> texts/vms/illustrations/EVA_Text
+
+
 # Unclassifiable pages as separate section
 $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QH +PE >> texts/vms/sections_unk/Unknown
 $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QH +PF >> texts/vms/sections_unk/Unknown
 $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QH +PG >> texts/vms/sections_unk/Unknown
 
-$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IA > texts/vms/sections/EVA_AstroCZ
-$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IC >> texts/vms/sections/EVA_AstroCZ
-$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IZ >> texts/vms/sections/EVA_AstroCZ
-$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IA > texts/vms/sections_unk/EVA_AstroCZ
-$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IC >> texts/vms/sections_unk/EVA_AstroCZ
-$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +IZ >> texts/vms/sections_unk/EVA_AstroCZ
 # f85r1 Text page on the same sheet as Cosmo diagrams
 $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QN +PB >> texts/vms/sections/EVA_AstroCZ
 # Unclassifiable pages as separate section
 $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QN +PB >> texts/vms/sections_unk/Unknown
 
-# +QT Quire13 / Bio
-$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QM > texts/vms/sections/EVA_Q13
-$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QM > texts/vms/sections_unk/EVA_Q13
 
-# +QT Quire20
-$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QT > texts/vms/sections/EVA_Q20
-$IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 -@L -f0 +QT > texts/vms/sections_unk/EVA_Q20
 
 
 for text_type in Circular Label Paragraph Radial
@@ -91,7 +99,7 @@ $IVTTDIR/ivtt $IVTTDIR/ZL_ivtff_1r.txt -a2 -c5 -h2 -s1 -u1 +@$lett -f0 |\
   grep -Ev '<!|#'|sed -e "s/[{}']//g"| sed -e 's/@[0-9][0-9]\([0-9]\);/\1/g' > texts/vms/text_type/$text_type
 done
   
-for f in texts/vms/all/* texts/vms/sections/* texts/vms/sections_unk/* texts/vms/text_type/*
+for f in texts/vms/all/* texts/vms/sections/* texts/vms/sections_unk/* texts/vms/text_type/* texts/vms/illustrations/*
 do
   # convert high ascii bytes to printable characters
   iconv -f Windows-1252 -t utf-8 $f > /tmp/conv
